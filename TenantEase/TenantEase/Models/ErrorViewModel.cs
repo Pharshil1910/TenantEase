d81a8fa78a553660fedd86f2a2dded44cdf83b6d
Harshil Kalsariya <pboom1930@gmail.com>
2024-11-09 21:25:03 +0530

    initial push

---
+++ TenantEase/TenantEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TenantEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TenantEase/TenantEase/Models/TenantEaseDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TenantEase.Models
+{
+    public class TenantEaseDBContext : DbContext
+    {
+        public TenantEaseDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
